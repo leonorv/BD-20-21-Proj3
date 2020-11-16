6e41d1a8d4122fbae0cc60154b03fd0f5f5209ba
leonorv <leonor.veloso@tecnico.ulisboa.pt>
2020-11-16 20:29:10 +0000

    addded schema

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
+/*timestamp format: 2018-9-20 23:59:59*/
 drop table regiao cascade;
 drop table concelho cascade;
 drop table instituicao cascade;
@@ -9,25 +10,92 @@ drop table venda_farmacia cascade;
 drop table instituicao cascade;
 drop table prescricao_venda cascade;
 
-create table regiao(
+create table regiao (
     num_regiao serial not null unique,
-    nome char(50) not null unique,
-    num_habitantes integer not null unique,
-    primary key(num_regiao);
+    nome char(50) not null unique check(nome in ('Norte', 'Centro', 'Lisboa', 'Alentejo', 'Algarve'),
+    num_habitantes integer unsigned not null unique,
+    primary key(num_regiao)
 );
 
-create table concelho(
+create table concelho (
     num_concelho serial not null unique,
     num_regiao serial not null unique,
     nome char(50) not null unique,
-    num_habitantes integer not null unique,
-    foreign key(num_regiao) references regiao(num_regiao) ON DELETE CASCADE,
-    primary key(num_concelho, num_regiao);
+    num_habitantes integer unsigned not null,
+    foreign key(num_regiao) references regiao(num_regiao) on delete cascade,
+    primary key(num_concelho, num_regiao)
 );
 
-create table instituicao(
+create table instituicao (
     nome char(50) not null unique,
-    
+    tipo char(20) not null check(tipo in ('farmacia', 'laboratorio', 'clinica', 'laboratorial')),
+    num_concelho serial not null unique,
+    num_regiao serial not null unique,
+    foreign key(num_concelho, num_regiao) references concelho(num_concelho, num_regiao) on delete cascade,
+    primary key(nome)
 );
 
+create table medico (
+    num_cedula serial not null unique,
+    nome char(50) not null, 
+    especialidade char(50) not null,
+    primary key(num_cedula)
+);
 
+create table consulta (
+    num_cedula serial not null,
+    num_doente serial not null,
+    dia_hora timestamp not null check(weekday(dia_hora) not in (5, 6)),
+    nome_instituicao not null,
+    foreign key(num_cedula) references medico(num_cedula) on delete cascade,
+    foreign key(nome_instituicao) references instituicao(nome) on delete cascade,
+    primary key(num_cedula, num_doente, dia_hora),
+    constraint RI_consulta_2 unique(num_doente, dia_hora, nome_instituicao)
+);
+
+create table prescricao (
+    num_cedula serial not null unique,
+    num_doente serial not null unique,
+    dia_hora timestamp not null,
+    substancia​ char(50) not null,
+    quant integer unsigned not null,
+    foreign key(num_cedula, num_doente, dia_hora) references consulta(num_cedula, num_doente, dia_hora) on delete cascade,
+    primary key(num_cedula, num_doente, dia_hora, substancia​)
+);
+
+create table analise (​
+    ​num_analise​ serial not null unique,
+    especialidade char(50) not null, 
+    num_cedula serial, 
+    num_doente serial, 
+    dia_hora timestamp, 
+    data_registo timestamp not null, 
+    nome char(50) not null, 
+    quant integer not null, 
+    inst char(50) not null,
+    foreign key(num_cedula, num_doente, dia_hora) references consulta(num_cedula, num_doente, dia_hora) on delete cascade,
+    foreign key(inst) references instituicao(nome) on delete cascade,
+    primary key(num_analise)
+    constraint RI_analise check((num_cedula null and num_doente null and dia_hora null) or check(consulta.especialidade == especialidade))
+);
+
+create table venda_farmacia (
+    num_venda serial not null unique​, 
+    data_registo timestamp not null, 
+    substancia char(50) not null,
+    quant integer not null, 
+    preco decimal(6,2) not null, 
+    foreign key(inst) references instituicao(nome) on delete cascade,
+    primary key(num_venda)
+);
+
+create table prescricao_venda (
+    num_cedula serial not null,
+    num_doente serial not null,
+    dia_hora timestamp not null,
+    substancia char(50) not null,
+    num_venda serial not null,
+    foreign key(num_venda) references venda_farmacia(num_venda),
+    foreign key(num_cedula, num_doente, dia_hora, substancia) references prescricao(num_cedula, num_doente, dia_hora, substancia),
+    primary key(num_cedula, num_doente, dia_hora, substancia, num_venda) 
+);
