adf976aa3df9949e9a41f275ebcf3326a7343fc4
leonorv <leonor.veloso@tecnico.ulisboa.pt>
2020-11-16 23:39:25 +0000

    added more queries

--- queries.sql
+++ queries.sql
@@ -7,8 +7,22 @@ having count(num_vendas) >= ALL
     (select count(num_vendas)
     from c natural join i natural join v
 	where v.data == current.date
-    group by num_concelho)
+    group by num_concelho);
 
+
+/*QUERY 2*/
+select m.nome,
+from medico as m natural join prescricao as p natural join regiao as r
+where data between '2019-01-01 00:00:00' and '2019-06-30 23:59:59';
+group by num_regiao
+having count(data) >= ALL
+    (select count(data) 
+    from m natural join p natural join r
+    where data between '2019-01-01 00:00:00' and '2019-06-30 23:59:59');
+
+/*QUERY 3*/
+select nome
+from medico as m natural join
 
 
 
